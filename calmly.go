aa9679376a2752cad0dbc136a508951267e9b92c
Alex <alex@alrux.com>
2016-04-14 18:55:42 -0400

    add 1.6.1 and 1.5.4, remove 1.4.x in travis-ci tests

--- calmly.go
+++ calmly.go
@@ -1,4 +1,4 @@
-// Copyright 2016 ALRUX Inc.
+// Copyright 2015 ALRUX Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
